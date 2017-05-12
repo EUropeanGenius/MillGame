4dac966c991bc6d30741395392d5933050440cfd
Alessandro Berlati <alessandro.berlati@gmail.com>
2017-05-12 16:09:58 +0200

    Hamburger list completed

--- src/game/rules/Hamburger.java
+++ src/game/rules/Hamburger.java
@@ -43,6 +43,145 @@ public class Hamburger {
             put("0,0", new ArrayList<short[][]>() {
                 {
                     add(new short[][] { {2,0},{1,0} });
+                    add(new short[][] { {6,0},{7,0} });
+                }
+            });
+            put("1,0", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {2,0},{0,0} });
+                    add(new short[][] { {1,1},{1,2} });
+                }
+            });
+            put("2,0", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {0,0},{1,0} });
+                    add(new short[][] { {3,0},{4,0} });
+                }
+            });
+            put("3,0", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {2,0},{4,0} });
+                    add(new short[][] { {3,2},{3,1} });
+                }
+            });
+            put("4,0", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {2,0},{3,0} });
+                    add(new short[][] { {6,0},{5,0} });
+                }
+            });
+            put("5,0", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {4,0},{6,0} });
+                    add(new short[][] { {5,1},{5,2} });
+                }
+            });
+            put("6,0", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {4,0},{5,0} });
+                    add(new short[][] { {0,0},{7,0} });
+                }
+            });
+            put("7,0", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {0,0},{6,0} });
+                    add(new short[][] { {7,1},{7,2} });
+                }
+            });
+            put("0,1", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {1,1},{2,1} });
+                    add(new short[][] { {7,1},{6,1} });
+                }
+            });
+            put("1,1", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {0,1},{2,1} });
+                    add(new short[][] { {1,0},{1,2} });
+                }
+            });
+            put("2,1", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {1,1},{0,1} });
+                    add(new short[][] { {4,1},{3,1} });
+                }
+            });
+            put("3,1", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {4,1},{2,1} });
+                    add(new short[][] { {3,0},{3,2} });
+                }
+            });
+            put("4,1", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {2,1},{3,1} });
+                    add(new short[][] { {5,1},{6,1} });
+                }
+            });
+            put("5,1", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {4,1},{6,1} });
+                    add(new short[][] { {5,0},{5,2} });
+                }
+            });
+            put("6,1", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {4,1},{5,1} });
+                    add(new short[][] { {7,1},{0,1} });
+                }
+            });
+            put("7,1", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {0,1},{6,1} });
+                    add(new short[][] { {7,0},{7,2} });
+                }
+            });
+            put("0,2", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {1,2},{2,2} });
+                    add(new short[][] { {7,2},{6,2} });
+                }
+            });
+            put("1,2", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {0,2},{2,2} });
+                    add(new short[][] { {1,0},{1,1} });
+                }
+            });
+            put("2,2", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {0,2},{1,2} });
+                    add(new short[][] { {3,2},{4,2} });
+                }
+            });
+            put("3,2", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {2,2},{4,2} });
+                    add(new short[][] { {3,0},{3,1} });
+                }
+            });
+            put("4,2", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {2,2},{3,2} });
+                    add(new short[][] { {5,2},{6,2} });
+                }
+            });
+            put("5,2", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {4,2},{6,2} });
+                    add(new short[][] { {5,0},{5,1} });
+                }
+            });
+            put("6,2", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {4,2},{5,2} });
+                    add(new short[][] { {7,2},{6,2} });
+                }
+            });
+            put("7,2", new ArrayList<short[][]>() {
+                {
+                    add(new short[][] { {0,2},{6,2} });
+                    add(new short[][] { {7,0},{7,1} });
                 }
             });
         }
@@ -51,9 +190,170 @@ public class Hamburger {
     //TODO
     public static final Map<String,List<short[]>> neighbors =  new HashMap<String, List<short[]>>() {
         {
+            //angoli esterni
             put("0,0", new ArrayList<short[]>() {
                 {
+                    add(new short[] {1,0});
+                    add(new short[] {7,0});
+                }
+            });
+            put("2,0", new ArrayList<short[]>() {
+                {
+                    add(new short[] {1,0});
+                    add(new short[] {3,0});
+                }
+            });
+            put("4,0", new ArrayList<short[]>() {
+                {
+                    add(new short[] {3,0});
+                    add(new short[] {5,0});
+                }
+            });
+            put("6,0", new ArrayList<short[]>() {
+                {
+                    add(new short[] {5,0});
+                    add(new short[] {7,0});
+                }
+            });
+            //angoli centrale
+            put("0,1", new ArrayList<short[]>() {
+                {
+                    add(new short[] {1,1});
+                    add(new short[] {7,1});
+                }
+            });
+            put("2,1", new ArrayList<short[]>() {
+                {
+                    add(new short[] {1,1});
+                    add(new short[] {3,1});
+                }
+            });
+            put("4,1", new ArrayList<short[]>() {
+                {
+                    add(new short[] {3,1});
+                    add(new short[] {5,1});
+                }
+            });
+            put("6,1", new ArrayList<short[]>() {
+                {
+                    add(new short[] {5,1});
+                    add(new short[] {7,1});
+                }
+            });
+            //angoli interno
+            put("0,2", new ArrayList<short[]>() {
+                {
+                    add(new short[] {1,2});
+                    add(new short[] {7,2});
+                }
+            });
+            put("2,2", new ArrayList<short[]>() {
+                {
+                    add(new short[] {1,2});
+                    add(new short[] {3,2});
+                }
+            });
+            put("4,2", new ArrayList<short[]>() {
+                {
+                    add(new short[] {3,2});
+                    add(new short[] {5,2});
+                }
+            });
+            put("6,2", new ArrayList<short[]>() {
+                {
+                    add(new short[] {5,2});
+                    add(new short[] {7,2});
+                }
+            });
+            //centrali esterno
+            put("1,0", new ArrayList<short[]>() {
+                {
+                    add(new short[] {0,0});
                     add(new short[] {2,0});
+                    add(new short[] {1,1});
+                }
+            });
+            put("3,0", new ArrayList<short[]>() {
+                {
+                    add(new short[] {2,0});
+                    add(new short[] {4,0});
+                    add(new short[] {3,1});
+                }
+            });
+            put("5,0", new ArrayList<short[]>() {
+                {
+                    add(new short[] {4,0});
+                    add(new short[] {6,0});
+                    add(new short[] {5,1});
+                }
+            });
+            put("7,0", new ArrayList<short[]>() {
+                {
+                    add(new short[] {6,0});
+                    add(new short[] {0,0});
+                    add(new short[] {7,1});
+                }
+            });
+            //centrali interno
+            put("1,2", new ArrayList<short[]>() {
+                {
+                    add(new short[] {0,2});
+                    add(new short[] {2,2});
+                    add(new short[] {1,1});
+                }
+            });
+            put("3,2", new ArrayList<short[]>() {
+                {
+                    add(new short[] {2,2});
+                    add(new short[] {4,2});
+                    add(new short[] {3,1});
+                }
+            });
+            put("5,2", new ArrayList<short[]>() {
+                {
+                    add(new short[] {4,2});
+                    add(new short[] {6,2});
+                    add(new short[] {5,1});
+                }
+            });
+            put("7,2", new ArrayList<short[]>() {
+                {
+                    add(new short[] {6,2});
+                    add(new short[] {0,2});
+                    add(new short[] {7,1});
+                }
+            });
+            //centrali centrale
+            put("1,1", new ArrayList<short[]>() {
+                {
+                    add(new short[] {0,1});
+                    add(new short[] {2,1});
+                    add(new short[] {1,0});
+                    add(new short[] {1,2});
+                }
+            });
+            put("3,1", new ArrayList<short[]>() {
+                {
+                    add(new short[] {2,1});
+                    add(new short[] {4,1});
+                    add(new short[] {3,0});
+                    add(new short[] {3,2});
+                }
+            });
+            put("5,1", new ArrayList<short[]>() {
+                {
+                    add(new short[] {4,1});
+                    add(new short[] {6,1});
+                    add(new short[] {5,0});
+                    add(new short[] {5,2});
+                }
+            });
+            put("7,1", new ArrayList<short[]>() {
+                {
+                    add(new short[] {6,1});
+                    add(new short[] {0,1});
+                    add(new short[] {7,0});
+                    add(new short[] {1,2});
                 }
             });
         }
